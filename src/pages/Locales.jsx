089140c6d234603f0bacc2a2612e58b15830b5e1
Andres Lopez <73659007+maitahlcor@users.noreply.github.com>
2025-08-25 07:45:26 -0500

    deploy

--- src/pages/Locales.jsx
+++ src/pages/Locales.jsx
@@ -1,40 +1,11 @@
-import { useState } from "react";
-import { api } from "../services/api";
-import { useNavigate } from "react-router-dom";
+import Survey from "../components/Survey";
+import preguntasUsuarios from "../data/encuestaLocales.json";
 
-export default function Locales() {
-  const [form, setForm] = useState({ l1: "" });
-  const navigate = useNavigate();
-
-  const onChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    await api.post("/locales", { respuestas: form });
-    alert("Guardado");
-    navigate("/");
-  };
-
+export default function Usuarios() {
   return (
-    <section className="card">
-      <h2>Encuesta: Locales</h2>
-
-      <form onSubmit={onSubmit} className="form">
-        <div className="field">
-          <label>¿El parqueadero de su local es suficiente?</label>
-          <select name="l1" onChange={onChange} required>
-            <option value="">Seleccione...</option>
-            <option>Siempre</option>
-            <option>A veces</option>
-            <option>Nunca</option>
-          </select>
-        </div>
-
-        <div className="actions end">
-          <button type="button" className="btn" onClick={() => navigate("/")}>Cancelar</button>
-          <button type="submit" className="btn btn-primary">Guardar</button>
-        </div>
-      </form>
-    </section>
+    <div>
+      <h1>Encuesta de Usuarios</h1>
+      <Survey questions={preguntasUsuarios} />
+    </div>
   );
 }
